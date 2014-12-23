5bae6c8ed4ae07a5d6d77729ab4fefd93f433e71
hazytint <hazytint@gmail.com>
2014-12-23 17:11:25 +0900

    Initial Commit

---
+++ Emoji-IME/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emoji-IME
+//
+//  Created by Wuhua Dai on 14/12/23.
+//  Copyright (c) 2014年 wua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Emoji-IMETests/Emoji_IMETests.swift
@@ -0,0 +1,36 @@
+//
+//  Emoji_IMETests.swift
+//  Emoji-IMETests
+//
+//  Created by Wuhua Dai on 14/12/23.
+//  Copyright (c) 2014年 wua. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Emoji_IMETests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
